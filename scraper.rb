9786a944c75361065e01e6fd6de06a32b970ec3d
Jan Zikan <jan.zikan@gmail.com>
2026-03-25 03:46:52 +0100

    Functional version of the script
    * Scrape product details pages
    * Save scraped data into CSV file

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'csv'
 require 'dotenv/load'
 require 'http'
 require 'json'
@@ -11,8 +12,7 @@ FIRECRAWL_API_KEY = ENV.fetch('FIRECRAWL_API_KEY')
 # Firecrawl API client
 class FirecrawlClient
   API_BASE_URL = 'https://api.firecrawl.dev'
-  API_ENDPOINT_SCRAPE = '/v2/scrape'
-  API_ENDPOINT_BATCH_SCRAPE = '/v2/batch/scrape'
+  API_ENDPOINT = '/v2/scrape'
 
   def initialize(api_key)
     headers = {
@@ -24,13 +24,7 @@ class FirecrawlClient
   end
 
   def scrape(url)
-    response = @client.post(API_ENDPOINT_SCRAPE, json: request_params(url: url))
-    JSON.parse(response.body)
-    # JSON.parse(File.read('firecrawl.json'))
-  end
-
-  def batch_scrape(urls)
-    response = @client.post(API_ENDPOINT_BATCH_SCRAPE, json: request_params(urls: urls))
+    response = @client.post(API_ENDPOINT, json: request_params(url: url))
     JSON.parse(response.body)
   end
 
@@ -42,16 +36,16 @@ class FirecrawlClient
 
   def default_request_params
     {
-      'onlyMainContent': true,
-      'mobile' => false,
-      'formats' => ['html'],
-      'location' => { 'country': 'CZ', 'languages': ['cs-CZ'] }
+      'onlyMainContent' => true,
+      'formats' => ['html']
     }
   end
 end
 
-# Extracts data from Allegro product listing and product details HTML
+# Extracts data from Allegro product list and product details HTML
 class AllegroParser
+  PRODUCT_CODE_PATTERN = /Kód produktu\s*:?\s*(.+?\[\*\(.+?\)\*\])/m
+
   def products(html)
     doc = Nokogiri::HTML(html)
     doc.css('article').map { |article| parse_product(article) }.compact
@@ -59,6 +53,17 @@ class AllegroParser
 
   def product_details(html)
     doc = Nokogiri::HTML(html)
+
+    description_el = doc.at_css('[itemprop="description"]')
+    description = description_el&.text&.strip
+    description = nil if description&.empty?
+
+    code_match = description&.match(PRODUCT_CODE_PATTERN)
+
+    {
+      product_code: code_match ? code_match[1] : nil,
+      description: description
+    }
   end
 
   private
@@ -87,18 +92,49 @@ end
 
 # Main routine
 
-SOURCE_URL = 'https://allegro.cz/kategorie/pocitace-notebooky-491?order=pd'
+SOURCE_URL = 'https://allegro.cz/kategorie/pocitace-notebooky-491?order=pd&p=%s'
 ITEMS_TO_SCRAPE = 1_000
 ITEMS_PER_PAGE = 60
 
-puts '=== Allegro.cz Scraper (Firecrawl) ==='
-puts
+CSV_FILE = 'products.csv'
+CSV_COLUMNS = %i[name url image_url price product_code description].freeze
 
-client = FirecrawlClient.new(FIRECRAWL_API_KEY)
-json = client.scrape(SOURCE_URL)
+puts '=== Allegro Scraper (Firecrawl) ==='
 
+scraper = FirecrawlClient.new(FIRECRAWL_API_KEY)
 parser = AllegroParser.new
-products = parser.products(json.dig('data', 'html'))
 
-File.write('products.json', JSON.pretty_generate(products))
-puts "Saved #{products.length} products to products.json"
+csv = CSV.open(CSV_FILE, 'w')
+csv << CSV_COLUMNS.map(&:to_s)
+
+# How many pages we need to scrape to get all the items
+pages_to_scrape = (ITEMS_TO_SCRAPE.to_f / ITEMS_PER_PAGE).ceil
+
+13.upto(pages_to_scrape) do |i|
+  product_list_url = SOURCE_URL % i
+  puts "Scraping product list page: #{product_list_url}"
+
+  # Scrape product list page
+  json = scraper.scrape(product_list_url)
+  products = parser.products(json.dig('data', 'html'))
+  puts "Products found: #{products.length}"
+
+  # Scrape product details pages
+  # We have to scrape pages one by one, because of the Firecrawl API rate limits
+  # https://docs.firecrawl.dev/rate-limits
+  puts "Scraping product details pages"
+  products.each do |product|
+    url = product[:url]
+    puts url
+
+    json = scraper.scrape(url)
+    details = parser.product_details(json.dig('data', 'html'))
+    data = product.merge(details)
+
+    csv << CSV_COLUMNS.map { |col| data[col] }
+    csv.flush
+  end
+end
+
+csv.close
+puts "Data saved to #{CSV_FILE}"
